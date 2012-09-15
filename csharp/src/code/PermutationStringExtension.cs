a12d47fd02371ad56720432249cea9bf162c643d
TerryH <terry@tnthughes.net>
2012-09-14 17:18:11 -0700

    Swap with multiple units

--- csharp/src/code/PermutationStringExtension.cs
+++ csharp/src/code/PermutationStringExtension.cs
@@ -1,5 +1,6 @@
 namespace Permutations
 {
+    using System;
     using System.Linq;
 
     public static class PermutationStringExtension
@@ -7,18 +8,21 @@ namespace Permutations
         public static string Permutate(this string @string, string expression)
         {
             var result = @string;
-            var indexes = expression.Substring(1, expression.Length - 2).Select(c => result.IndexOf(c)).ToArray();
+            var units = expression.Split(new[] { '(', ')' }, StringSplitOptions.RemoveEmptyEntries);
 
-            for (var i = 0; i < indexes.Length - 1; i++)
+            foreach (var indexes in units.Select(unit => unit.Select(c => result.IndexOf(c)).ToArray()))
             {
-                var firstCharacter = result[indexes[i]];
-                var secondCharacter = result[indexes[i + 1]];
+                for (var i = 0; i < indexes.Length - 1; i++)
+                {
+                    var firstCharacter = result[indexes[i]];
+                    var secondCharacter = result[indexes[i + 1]];
 
-                result = result
-                    .Replace(secondCharacter, '*')
-                    .Replace(firstCharacter, secondCharacter)
-                    .Replace('*', firstCharacter)
-                    ;
+                    result = result
+                        .Replace(secondCharacter, '*')
+                        .Replace(firstCharacter, secondCharacter)
+                        .Replace('*', firstCharacter)
+                        ;
+                }
             }
 
             return result;
